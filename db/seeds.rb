6e37d74f144737be0255179ef9601d41fa8d3761
Naomi Arefaine <naomiarefaine@gmail.com>
2020-02-19 20:01:08 +0000

    Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: 'Hong kong', address: '8th st, London', phone_number: '01 43 54 23 31', category: 'chinese')
+Restaurant.create(name: 'Mama Mia', address: '9th St, Oxford', phone_number: '02 43 55 23 31', category: 'italian')
+Restaurant.create(name: 'Sushi-mushi', address: '10th St, Essex', phone_number: '01 43 54 22 32', category: 'japanese')
+Restaurant.create(name: 'EehooEehoo', address: '1st St, London', phone_number: '03 44 54 21 32', category: 'french')
+Restaurant.create(name: 'Fries myeyes', address: '3rd Rd, London', phone_number: '03 44 54 21 33', category: 'belgian')
